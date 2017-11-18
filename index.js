cdc2a3a2b72029c5166996a642315ef0838865ce
lloisborn <faisalmorensya@gmail.com>
2017-11-19 03:05:35 +0700

    using react-materialize

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App.jsx';
+import Materialize from 'materialize-css/dist/css/materialize.css';
 
 ReactDOM.render(
   <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
   entry: './index.js',
   output: {
@@ -11,6 +14,34 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
     ],
   },
 };
