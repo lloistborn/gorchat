27c2e4f5867db7eb045b85699cbfc7dacbca433d
lloisborn <faisalmorensya@gmail.com>
2017-11-18 08:58:20 +0700

    more on eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        preLoaders: [
-            {
-                test:/\.js$/,
-                exclude: '/node_modules',
-                loader: 'eslint-loader'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: '/node_modules/'
-            },
-            {
-                test: /\.js$/,
-                loader: ['eslint-loader', 'babel-loader'],
-                exclude: '/node_modules'
-            }
-        ]
-    }
-}
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules',
+        loader: 'eslint-loader',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.js$/,
+        loader: ['eslint-loader', 'babel-loader'],
+        exclude: '/node_modules',
+      },
+    ],
+  },
+};
